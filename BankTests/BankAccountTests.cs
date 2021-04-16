df26fe2e39440e4a1af43f08d28324d4e552286b
Kevin Panko <pankok@gmail.com>
2021-04-16 18:11:48 -0400

    added a test

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -8,8 +8,20 @@ namespace BankTests
     public class BankAccountTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Debit_WithValidAmount_UpdatesBalance()
         {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
     }
 }
